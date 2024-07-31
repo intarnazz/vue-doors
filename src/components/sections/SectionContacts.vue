4ce72327e231ae9c11612634728d6f09a42c8999
Ortadox <misha.vidiakin@gmail.com>
2024-07-31 13:53:56 +0300

    contacts

--- src/components/sections/SectionContacts.vue
+++ src/components/sections/SectionContacts.vue
@@ -1,12 +1,16 @@
-<script setup></script>
+<script setup>
+import LayoutWrapper from '@/layout/LayoutWrapper.vue'
+</script>
 
 <template>
   <section class="contacts">
-    <h2 class="source">Find your next designer today</h2>
-    <p class="sans">
-      Get inspired by the work of millions of top-rated designers & agencies around the world.
-    </p>
-    <button class="button">button</button>
+    <LayoutWrapper class="flex box-x contacts__wrapper">
+      <h2 class="source">Find your next designer today</h2>
+      <p class="sans">
+        Get inspired by the work of millions of top-rated designers & agencies around the world.
+      </p>
+      <button class="button">button</button>
+    </LayoutWrapper>
   </section>
 </template>
 
@@ -14,8 +18,9 @@
 .contacts
   background-color: $mainColor
   padding: 2.9rem 0
-  display: flex
-  flex-direction: column
-  align-items: center
-  gap: 2rem
+  &__wrapper
+    display: flex
+    flex-direction: column
+    align-items: center
+    gap: 2rem
 </style>
