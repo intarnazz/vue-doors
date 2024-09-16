fb3c0a03fa3108d28a039c710535c6abd281109c
ortadox <misha.vidiakin@gmail.com>
2024-09-16 17:54:20 +0300

    search

--- src/components/ComponentSearch.vue
+++ src/components/ComponentSearch.vue
@@ -4,16 +4,18 @@ import FormMain from '@/layout/form/FormMain.vue'
 
 <template>
   <FormMain>
-    <input
-      v-model="text"
-      class="flex"
-      id="search"
-      name="search"
-      type="search"
-      placeholder="search..."
-      autocomplete="off"
-    />
-    <img src="@/assets/icons/search.svg" alt="search" />
+    <div class="box-x">
+      <input
+        v-model="text"
+        class="flex"
+        id="search"
+        name="search"
+        type="search"
+        placeholder="search..."
+        autocomplete="off"
+      />
+      <img src="@/assets/icons/search.svg" alt="search" />
+    </div>
   </FormMain>
 </template>
 
